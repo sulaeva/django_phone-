45a7a4210a31c1534efdc60255fa7d4a4d327260
sulaevatumar67-sys <sulaevatumar67@gmail.com>
2025-12-27 17:02:35 +0600

    django

--- phone/admin.py
+++ phone/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Phone
 
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'price')
+    list_filter = ('brand', 'price', 'model')
+    search_fields = ('brand', 'model', 'price')
 
 
-admin.site.register(Phone)
+admin.site.register(Phone, PhoneAdmin)
+
